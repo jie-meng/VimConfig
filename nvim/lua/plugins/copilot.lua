22ffeae9abe749f9caf9a2d3151be74ca06af120
Jie Meng <jmengxy@gmail.com>
2025-07-25 13:19:12 +0800

    Use new copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,40 +1,92 @@
 -- ============================================================================
--- GitHub Copilot - AI pair programming
+-- GitHub Copilot - AI pair programming with Chat support
 -- ============================================================================
 
 return {
+  -- Main Copilot plugin
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
     lazy = false,
+    cmd = "Copilot",
+    event = "InsertEnter",
     config = function()
-      -- Disable default tab mapping to avoid conflicts
-      vim.g.copilot_no_tab_map = true
-      
-      -- Set up key mappings
-      vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false,
-        desc = "Accept Copilot suggestion"
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>"
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<Tab>",
+            accept_word = false,
+            accept_line = false,
+            next = "<C-j>",
+            prev = "<C-k>",
+            dismiss = "<C-l>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
       })
-      
-      vim.keymap.set('i', '<C-i>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false,
-        desc = "Accept Copilot suggestion (alternative)"
-      })
-      
-      -- These mappings are only active when Copilot has suggestions
-      vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', { desc = "Next Copilot suggestion" })
-      vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', { desc = "Previous Copilot suggestion" })
-      vim.keymap.set('i', '<C-l>', '<Plug>(copilot-dismiss)', { desc = "Dismiss Copilot suggestion" })
-      
-      -- Command mappings (matching original .vimrc)
-      vim.keymap.set('n', '<space>ae', ':Copilot enable<CR>', { desc = "Enable Copilot" })
-      vim.keymap.set('n', '<space>ad', ':Copilot disable<CR>', { desc = "Disable Copilot" })
-      vim.keymap.set('n', '<space>as', ':Copilot status<CR>', { desc = "Copilot status" })
-      vim.keymap.set('n', '<space>ap', ':Copilot panel<CR>', { desc = "Copilot panel" })
-      vim.keymap.set('n', '<space>au', ':Copilot setup<CR>', { desc = "Copilot setup" })
-      vim.keymap.set('n', '<space>ao', ':Copilot signout<CR>', { desc = "Copilot signout" })
     end,
+  },
+  
+  -- Copilot Chat plugin
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "main",
+    lazy = false,
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, 
+      { "nvim-lua/plenary.nvim" }, 
+    },
+    config = function()
+      require("CopilotChat").setup({
+        debug = false, -- Enable debugging
+        -- See Configuration section for rest
+      })
+    end,
+    keys = {
+      -- Chat commands
+      { "<space>cc", ":CopilotChat<CR>", desc = "Open Copilot Chat" },
+      { "<space>ce", ":CopilotChatExplain<CR>", desc = "Explain code" },
+      { "<space>ct", ":CopilotChatTests<CR>", desc = "Generate tests" },
+      { "<space>cf", ":CopilotChatFix<CR>", desc = "Fix code" },
+      { "<space>co", ":CopilotChatOptimize<CR>", desc = "Optimize code" },
+      { "<space>cd", ":CopilotChatDocs<CR>", desc = "Generate docs" },
+      { "<space>cr", ":CopilotChatReview<CR>", desc = "Review code" },
+      { "<space>cs", ":CopilotChatCommit<CR>", desc = "Generate commit message" },
+
+      -- Panel commands (keeping original functionality)
+      { "<space>ae", ":Copilot enable<CR>", desc = "Enable Copilot" },
+      { "<space>ad", ":Copilot disable<CR>", desc = "Disable Copilot" },
+      { "<space>as", ":Copilot status<CR>", desc = "Copilot status" },
+      { "<space>ap", ":Copilot panel<CR>", desc = "Copilot panel" },
+    },
   },
 }
