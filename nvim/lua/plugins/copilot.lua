0c428b14e84cdf50a0193d2b03ebc212e0094f8d
Jie Meng <jmengxy@gmail.com>
2025-07-25 09:54:53 +0800

    Fix copilot keymapping

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -10,15 +10,22 @@ return {
       vim.g.copilot_no_tab_map = true
       
       -- Set up key mappings
-      vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
+      vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false,
         desc = "Accept Copilot suggestion"
       })
       
-      vim.keymap.set('i', '<C-]>', '<Plug>(copilot-next)', { desc = "Next Copilot suggestion" })
-      vim.keymap.set('i', '<C-[>', '<Plug>(copilot-previous)', { desc = "Previous Copilot suggestion" })
-      vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-dismiss)', { desc = "Dismiss Copilot suggestion" })
+      vim.keymap.set('i', '<C-i>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+        desc = "Accept Copilot suggestion (alternative)"
+      })
+      
+      -- These mappings are only active when Copilot has suggestions
+      vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', { desc = "Next Copilot suggestion" })
+      vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', { desc = "Previous Copilot suggestion" })
+      vim.keymap.set('i', '<C-l>', '<Plug>(copilot-dismiss)', { desc = "Dismiss Copilot suggestion" })
       
       -- Command mappings (matching original .vimrc)
       vim.keymap.set('n', '<space>ae', ':Copilot enable<CR>', { desc = "Enable Copilot" })
