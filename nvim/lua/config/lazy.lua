f33889f5e51e3d805052ad7282d290b401870b09
Jie Meng <jmengxy@gmail.com>
2025-07-26 14:15:40 +0800

    lazy can update by selection

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,7 +21,11 @@ require("lazy").setup("plugins", {
     version = false,
   },
   install = { colorscheme = { "gruvbox" } },
-  checker = { enabled = true },
+  checker = { 
+    enabled = true,
+    notify = true,  -- Show notification when updates are available
+    frequency = 3600, -- Check every hour
+  },
   performance = {
     rtp = {
       disabled_plugins = {
@@ -34,3 +38,32 @@ require("lazy").setup("plugins", {
     },
   },
 })
+
+-- Auto-update prompt and keymaps
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyCheck",
+  callback = function()
+    -- Show a user-friendly update prompt
+    vim.defer_fn(function()
+      local lazy = require("lazy")
+      local updates = lazy.stats().updates
+      if updates > 0 then
+        local choice = vim.fn.confirm(
+          string.format("Found %d plugin updates. Update now?", updates),
+          "&Yes\n&No\n&Show details", 1
+        )
+        if choice == 1 then
+          vim.cmd("Lazy update")
+        elseif choice == 3 then
+          vim.cmd("Lazy")
+        end
+      end
+    end, 1000) -- Wait 1 second after startup
+  end,
+})
+
+-- Quick update keymaps
+vim.keymap.set("n", "<leader>Lu", ":Lazy update<CR>", { desc = "Lazy Update" })
+vim.keymap.set("n", "<leader>Ls", ":Lazy sync<CR>", { desc = "Lazy Sync" })
+vim.keymap.set("n", "<leader>Lc", ":Lazy check<CR>", { desc = "Lazy Check" })
+vim.keymap.set("n", "<leader>Ll", ":Lazy<CR>", { desc = "Lazy Menu" })
