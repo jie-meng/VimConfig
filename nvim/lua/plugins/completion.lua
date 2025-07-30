37c9235c9926a5d1d03dcec05bdbad47943d817a
Jie Meng <jmengxy@gmail.com>
2025-07-30 09:39:58 +0800

    Completion support Tab, S-Tab selection; Esc can close completion without to normal mode

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -38,11 +38,20 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
+        ["<Esc>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.close()
+          else
+            fallback()
+          end
+        end, { "i" }),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
