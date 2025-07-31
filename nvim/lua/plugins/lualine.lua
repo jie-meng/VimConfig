51c9f21a71aecd6983035eef0406f5ba1b6343ba
Jie Meng <jmengxy@gmail.com>
2025-07-31 09:43:42 +0800

    Fix lualine not show full path bug

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,7 +9,16 @@ return {
     if ok and cs and cs.apply_theme then
       cs.apply_theme(cs.current_idx)
     else
-      require("lualine").setup({})
+      require("lualine").setup({
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { { 'filename', path = 3 } },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+      })
     end
   end,
 }
