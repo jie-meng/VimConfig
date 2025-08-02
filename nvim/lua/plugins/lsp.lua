c8758839a605fde241187c3bf9d9c8e9e3228c94
Jie Meng <jmengxy@gmail.com>
2025-08-02 22:05:11 +0800

    lsp support esp-clangd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -164,9 +164,15 @@ return {
         pyright = {},
         clangd = {
           cmd = {
-            "clangd",
+           "clangd",
+            "--compile-commands-dir=.",
             "--background-index",
-            "--compile-commands-dir=.vim",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=google",
+            "--query-driver=/usr/bin/clang*,/usr/bin/g*,/usr/local/bin/clang*,/usr/local/bin/g*," .. home .. "/.espressif/tools/esp-clang/*/esp-clang/bin/*", 
           },
           filetypes = { "c", "cpp", "objc", "objcpp", "cc", "cxx" },
         },
