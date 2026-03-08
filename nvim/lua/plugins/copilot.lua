cddef2386da238eced679df29b15cea966905357
Jie Meng <jmengxy@gmail.com>
2026-03-08 14:10:52 +0800

    copilot accept only use C-l

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -44,23 +44,6 @@ return {
         desc = "Auto-reset Copilot suggestion highlight after colorscheme change"
       })
 
-      -- Smart Tab: accept Copilot suggestion if available, otherwise normal Tab behavior
-      vim.keymap.set("i", "<Tab>", function()
-        local suggestion = vim.fn["copilot#GetDisplayedSuggestion"]()
-        if suggestion.text ~= "" then
-          return vim.fn["copilot#Accept"]("")
-        elseif vim.fn.pumvisible() == 1 then
-          return "<C-n>"
-        else
-          local sw = vim.bo.shiftwidth
-          if vim.bo.expandtab then
-            return string.rep(" ", sw)
-          else
-            return "\t"
-          end
-        end
-      end, { expr = true, silent = true, desc = "Accept Copilot or indent at cursor" })
-
       vim.keymap.set("i", "<C-l>", 'copilot#Accept("")', { expr = true, replace_keycodes = false, desc = "Accept Copilot suggestion" })
       vim.keymap.set("i", "<C-.>", "<Plug>(copilot-next)", { desc = "Next Copilot suggestion" })
       vim.keymap.set("i", "<C-,>", "<Plug>(copilot-previous)", { desc = "Previous Copilot suggestion" })
